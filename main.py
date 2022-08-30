9c6db1fe6f3c2d3a23fbef9e1cdecd58e194d0a5
alex giovannini <alexgiovannini@alexs-MacBook-Air.local>
2022-08-30 13:34:41 -0700

    Added lists and dictionary

---
+++ main.py
@@ -0,0 +1,5 @@
+destinations = ["SLO", "Santa Barbara", "Las Angeles", "Auburn"]
+modes_of_transportation = ["Plane", "Train", "Boat", "Motorcycle"]
+restaurants = ["Italian", "Sushi", "Thai", "Mexican"]
+entertainment = ["Body of water", "Movie", "Hike", "Yoga class"]
+day_trip_dictionary = {}
